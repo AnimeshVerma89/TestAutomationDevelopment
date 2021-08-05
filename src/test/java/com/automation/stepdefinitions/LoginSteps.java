546798f7a4fd45b8832166606b6c58c0309ee163
Animesh <AnimeshVerma89>
2021-08-05 20:04:37 +0530

    Added Login.feature file along with related PO and StepDefinition files

---
+++ src/test/java/com/automation/stepdefinitions/LoginSteps.java
@@ -0,0 +1,71 @@
+package com.automation.stepdefinitions;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+import com.automation.pageobject.MyStorePage;
+
+import cucumber.api.java.en.*;
+
+public class LoginSteps {
+	public WebDriver driver = null;
+	MyStorePage myStorePage = null;
+	
+	@Given("User has launched chrome browser")
+	public void user_has_launched_chrome_browser() {
+	    // Write code here that turns the phrase above into concrete actions
+		System.setProperty("webdriver.chrome.driver","D:\\MyWorkspace\\HybridAutomationFramework\\Drivers\\chromedriver.exe");  
+	    // Instantiate a ChromeDriver class.       
+	     driver=new ChromeDriver();
+	     myStorePage = new MyStorePage(driver);
+	    
+	}
+
+	@When("User navigates to the Application URL {string}")
+	public void user_navigates_to_the_Application_URL(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+		driver.manage().window().maximize();
+	    driver.get(string);
+	    Assert.assertEquals(driver.getTitle(), "My Store", "User navigates to Incorrect page");
+	}
+
+	@When("Clicks Signin link on the {string} page")
+	public void clicks_Signin_link_on_the_page(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	   myStorePage.clickSignInLink();
+	}
+
+	@Then("Navigates to {string} Page")
+	public void navigates_to_Page(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@When("Enters username as {string} and password as {string}")
+	public void enters_username_as_and_password_as(String string, String string2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@When("Clicks on SignIn button on the {string} page")
+	public void clicks_on_SignIn_button_on_the_page(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@Then("User validates Account Information page")
+	public void user_validates_Account_Information_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@When("Clicks Signout link on the {string} page")
+	public void clicks_Signout_link_on_the_page(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+
+
+}
--- src/test/java/sampleTest/MyProgram.java
+++ src/test/java/sampleTest/MyProgram.java
@@ -1,11 +1,18 @@
 package sampleTest;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class MyProgram {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("My First java program");
+		System.setProperty("webdriver.chrome.driver","D:\\MyWorkspace\\HybridAutomationFramework\\Drivers\\chromedriver.exe");  
+	      
+	       // Instantiate a ChromeDriver class.       
+	    WebDriver driver=new ChromeDriver();
+	    driver.get("http://automationpractice.com/index.php");
 	}
 
 }
